8631d935a5985cf22a3518bc9aaf52ca9af31be5
Vansh Goel <goelvansh82@gmail.com>
2023-07-05 14:12:29 +0530

    Iniial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+const chats  = require('./data/data.js');
+
+app.get('/', (req, res) => {
+    res.send("API is working properly");
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    const Singlechat = chats.find((c) => c._id === req.params.id);
+    res.send(Singlechat);
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Server started on port 3000'));
